6f303fd5309e650ce9b5e3ae9f0bc9b6fbb4ad20
firo <cfiro@qq.com>
2022-01-15 12:22:29 +0800

    响应式

--- 2. 学习代理和反射/raft.js
+++ 2. 学习代理和反射/raft.js
@@ -6,12 +6,13 @@ let a = 1;
 // a.__proto__.a = 1
 console.log(a.__proto__);// 没有autobox 转化 就是原始属性上的__proto__肯定屁都没有 a.__proto__={}
 console.info(a.a, a.toString()); // 这里才使用了 相当于new Number(1).toString()
-
+console.log('--------------------------------------------');
 let b = new Number(2)
 console.log(b);
-console.info(new Number(b).toString());
+// console.info(new Number(b).toString());
 
 //[[Prototype]]: Number原始属性了
-new Number(b).__proto__.abc = 1
-console.log(new Number(b).constructor);
-console.log(b.abc);
+// new Number(b).__proto__.abc = 1
+// new Number(b).__proto__为object原型 也就是空对象
+console.log(b.prototype);
+
---
+++ 3. 手写vue3响应式/app.js
@@ -0,0 +1,64 @@
+const depsMap = new Map();
+
+// 追踪key值的变换，每一个key都有一个Set来添加更新函数
+// 此Set非彼set 这里的Set是一个集合
+function track(Key) {
+  console.log('track触发了');
+  let dep = depsMap.get(Key);
+  if (!dep) {
+    depsMap.set(Key, (dep = new Set()));
+  }
+  dep.add(effect);
+}
+
+// 触发器，用来触发更新函数
+function trigger(Key) {
+  console.log('trigger触发了');
+  let dep = depsMap.get(Key);
+  if (dep) {
+    dep.forEach((effect) => {
+      effect();
+    });
+  }
+}
+
+// 通过es6的新特性proxy和reflect来完成响应式
+function VueReactive(target){
+    const handler = {
+        get(target, Key, receiver) {
+            console.log("GET被调用了 " + Key);
+            // do other thing...
+            // receiver来表明this指向的对象
+            let result = Reflect.get(target, Key, receiver); // 结果
+            track(Key)
+            return result
+          },
+          set(target, Key, value, receiver) {
+            console.log("SET被调用了 " + Key);
+            // do other thing...
+            let result = Reflect.set(target, Key, value, receiver); // true
+            trigger(Key)
+            return result
+          },
+    }
+    // 使用代理来完成对对象的绑定和操作 target就是对象 handler就是操作
+    return new Proxy(target,handler)
+}
+
+// 设置的实例部分
+
+let product = VueReactive({ price: 5, quantity: 2 });
+console.log('---------------------------------------------------------');
+let total = 0;
+
+// 业务逻辑部分
+let effect = () => {
+    // 在这里就调用了两次GET price quantity
+  total = product.price * product.quantity;
+};
+
+effect();
+console.log(total);
+console.log('---------------------------------------------------------');
+product.quantity = 3  //SET被调用了 quantity
+console.log(total);
